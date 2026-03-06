d0f426520ea40d25e64e04aa65c0e7a2f7523a97
safetyshare <vivo2198256468@gmail.com>
2026-03-06 18:00:34 +0800

    各种运算符

--- Mar06/example3.java
+++ Mar06/example3.java
@@ -7,6 +7,14 @@ public class example3 {
         System.out.println(5.0/2);
         System.out.println(5.2%2);//浮点数是有存储误差的
         //两个整数可以求余，浮点数也可以求余运算。
-        
+        int x=5,y=7;
+        int z;
+        z=(x++)+y;//5+7=12
+        System.out.println(z);
+        x=5;z=++x+y;//x先自加变成6，6+7=13
+        System.out.println(z);
+        x=5;
+        z=-x++;
+        System.out.println(z);
     }
 }
