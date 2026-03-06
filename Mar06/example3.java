4bd1e34b0b78fc16ee6be57bfa49d930cad2bee9
safetyshare <vivo2198256468@gmail.com>
2026-03-06 17:11:09 +0800

    算数运算符

---
+++ Mar06/example3.java
@@ -0,0 +1,12 @@
+public class example3 {
+    public static void main(String[] args) {
+        System.out.println(3 +5);//结果8
+        System.out.println("hello" +5);//结果 hello5
+        System.out.println("hello"+ 3 + 5);//结果hello35
+        System.out.println(5/2);
+        System.out.println(5.0/2);
+        System.out.println(5.2%2);//浮点数是有存储误差的
+        //两个整数可以求余，浮点数也可以求余运算。
+        
+    }
+}
