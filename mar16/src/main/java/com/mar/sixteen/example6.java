55f0f412c07e701f8f9367ea6d79d9cabf460104
safetyshare <vivo2198256468@gmail.com>
2026-03-18 18:05:36 +0800

    新建第三课Java项目

---
+++ mar16/src/main/java/com/mar/sixteen/example6.java
@@ -0,0 +1,23 @@
+package com.mar.sixteen;
+
+public class example6 {
+    static void main(){
+        int i,sum=0;
+        i=1;
+/*        while (i<=100){
+            sum+=i;
+            i++;
+        }*/
+        do {
+            sum+=i;
+            i++;
+        }
+        while(i<=100);
+        System.out.println("和为："+sum);
+
+        for(i=1,sum=0;i<=100;i++){
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+}
---
+++ mar16/src/main/java/com/mar/sixteen/example7.java
@@ -0,0 +1,19 @@
+package com.mar.sixteen;
+
+public class example7 {
+    public static void main(String[] args) {
+        int i=200,sum=0;
+        while (i<=800)
+        {
+            if(i%7==0||i%9==0) {
+                sum+=i;
+                i++;
+            }
+            else {
+                i++;
+                continue;
+            }
+        }
+        System.out.println("结果为："+sum);
+    }
+}
