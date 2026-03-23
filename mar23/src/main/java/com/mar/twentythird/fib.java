752e5c346ccf3114f2601fa57e6fb1f85147efa9
safetyshare <vivo2198256468@gmail.com>
2026-03-23 11:53:38 +0800

    数列

--- mar23/src/main/java/com/mar/twentythird/fib.java
+++ mar23/src/main/java/com/mar/twentythird/fib.java
@@ -1,4 +1,20 @@
 package com.mar.twentythird;
 
 public class fib {
+    static void main() {
+        int[] shulie;
+        shulie=new int[20];
+        shulie[0]=1;
+        shulie[1]=1;
+        for(int i=2;i<shulie.length;i++){
+            shulie[i]=shulie[i-1]+shulie[i-2];
+        }
+        for (int i=0 ;i<shulie.length;i++) {
+            System.out.print(shulie[i]+"\t");
+            if ((i+1)%8==0) {
+                System.out.println();
+                
+            }
+        }
+    }
 }
