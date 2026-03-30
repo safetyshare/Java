35626efb51b767ac6f89294268e012e9326bd09c
safetyshare <vivo2198256468@gmail.com>
2026-03-30 11:52:18 +0800

    提交作业

--- mar30/src/main/java/com/mar/thirty/Test2.java
+++ mar30/src/main/java/com/mar/thirty/Test2.java
@@ -2,14 +2,25 @@ package com.mar.thirty;
 
 public class Test2 {
     static void main(){
-        Box box=new Box();
-        box.toString();
+        Box box1=new Box(3,4,6);
+        System.out.println("体积是："+box1.v());
+        System.out.println("表面积是："+box1.s());
+        System.out.println(box1.toString());
+        Box box2=new Box(5,7,8);
+        System.out.println("体积是："+box2.v());
+        System.out.println("表面积是："+box2.s());
+        System.out.println(box2.toString());
     }
 }
 class Box{
     private int length;
     private int width;
     private int height;
+    public Box(int a,int b,int h){
+        length=a;
+        width=b;
+        height=h;
+    }
     public int v(){
         int v;
         v=length*width*height;
@@ -23,8 +34,8 @@ class Box{
     public String toString(){
         String vs="体积为："+v();
         String ss="表面积为："+s();
-        System.out.println(vs);
-        System.out.println(ss);
-        return vs+"\n"+ss;
+//        System.out.println(vs);
+//        System.out.println(ss);
+        return vs+"\t"+ss;
     }
 }
