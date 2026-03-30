34b430b6e20989cb1b95fca544619541621a1955
safetyshare <vivo2198256468@gmail.com>
2026-03-30 11:37:46 +0800

    添加构造方法

--- mar30/src/main/java/com/mar/thirty/Test1.java
+++ mar30/src/main/java/com/mar/thirty/Test1.java
@@ -1,8 +1,25 @@
 package com.mar.thirty;
 
 public class Test1 {
-    static void mian(){
-
+    static void main(){
+        // Rect rect=new Rect();
+        // rect.area();
+        Person p1;//声明对象
+        p1 = new Person();//创建对象。只针对成员变量进行内存分配。
+        p1.think();
+        p1.say();//通过对象完成相应的功能
+        Rect re1=new Rect(2,3);
+        System.out.println(re1.clength());
+        System.out.println(re1.area());
+        Rect re2=new Rect(10,20);
+        System.out.println(re2.clength());
+        System.out.println(re2.area());
+        Yuan y1=new Yuan(5);
+        System.out.println(y1.clength());
+        System.out.println(y1.area());
+        Yuan y2=new Yuan(10);
+        System.out.println(y2.clength());
+        System.out.println(y2.area());
     }
 }
 //人类
@@ -20,8 +37,13 @@ class Person {
     }
 }
 class Rect {
-    private int length1;//长
-    private int weight;
+    private int length1=4;//长
+    private int weight=8;
+    //构造方法
+    public Rect(int l,int w){
+        length1=l;
+        weight=w;
+    }
     //成员方法
     //求周长
     public int clength(){
@@ -37,6 +59,9 @@ class Rect {
 }
 class Yuan {
     private int r;//半径
+    public Yuan(int r1){
+        r=r1;
+    }
     public double clength(){
         double cl;
         cl=2*Math.PI*r;
@@ -47,6 +72,4 @@ class Yuan {
         s=Math.PI*r*r;
         return s;
     }
-
-    
 }
