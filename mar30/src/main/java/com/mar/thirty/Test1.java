18c5fae589cac090d91b5f22ed627df92e2879f1
safetyshare <vivo2198256468@gmail.com>
2026-03-30 09:39:14 +0800

    面向对象编程

---
+++ mar30/src/main/java/com/mar/thirty/Test1.java
@@ -0,0 +1,26 @@
+package com.mar.thirty;
+
+public class Test1 {
+    static void mian(){
+
+    }
+}
+//人类
+class Person {
+    //成员变量：静态属性和特征
+    String name; //姓名
+    int h;//身高
+    int w;//体重
+    //成员方法：描述对象，具备的功能，能干什么，
+    public void think(){
+        System.out.println("人会思考！！");
+    }
+    public void say(){
+        System.out.println("人会说话！");
+    }
+}
+class Rect {
+    
+
+    
+}
