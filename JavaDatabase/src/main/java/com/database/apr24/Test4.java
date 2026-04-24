597be2833233476ec8039ad7748c344f4b4fcad1
safetyshare <vivo2198256468@gmail.com>
2026-04-24 17:08:38 +0800

    时间戳

--- JavaDatabase/src/main/java/com/database/apr24/Test4.java
+++ JavaDatabase/src/main/java/com/database/apr24/Test4.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Test4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int x;
+        int x,s=0;
+        long ms;
         System.out.println("inout x:");
         x=sc.nextInt();
         if (x==1) {
@@ -12,6 +13,16 @@ public class Test4 {
             System.out.println("程序继续执行");
             System.out.println("程序继续执行");
             System.out.println("程序继续执行");
+            ms=System.currentTimeMillis();
+            long ms1,ms2;
+            ms1=System.currentTimeMillis();
+            System.out.println(ms);
+            for (int i = 0; i < 100000; i++) {
+                s+=i-29999;
+            }
+            ms2=System.currentTimeMillis();
+            System.out.println("循环花费的时间："+(ms2-ms1));
         }
+        
     }
 }
