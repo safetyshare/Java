23c22514625d6163e4e7de851ace863f72c3359f
safetyshare <vivo2198256468@gmail.com>
2026-05-18 13:15:32 +0800

    缓冲区

---
+++ Advanced/src/main/java/com/advanced/io/buffcopytest.java
@@ -0,0 +1,37 @@
+package com.advanced.io;
+
+import java.io.*;
+
+public class buffcopytest {
+    public static void main(String[] args) throws FileNotFoundException {
+//        FileInputStream fis = null;
+//        FileOutputStream fos = null;
+        long start = System.currentTimeMillis();
+        int c = 0;
+        // BufferedInputStream bis=new BufferedInputStream(fis);
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream("src\\main\\resources\\test.txt"));
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("src\\main\\resources\\2.txt"));
+//        byte[] buff = new byte[1024];
+        while (true) {
+            try {
+                if (!((c = bis.read()) != -1))
+                    break;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                bos.write(c);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        try {
+            bis.close();
+            bos.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("复制文件耗时：" + (end - start) + "ms");
+    }
+}
--- Advanced/src/main/java/com/advanced/io/copytest.java
+++ Advanced/src/main/java/com/advanced/io/copytest.java
@@ -6,7 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class copytest {
-    static void main() {
+    public static void main(String[] args) {
         FileInputStream fis= null;
         FileOutputStream fos= null;
         try {
@@ -15,15 +15,17 @@ public class copytest {
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
+        long start=System.currentTimeMillis();
         int c=0;
+        byte[] buff=new byte[1024];
         while (true){
             try {
-                if (!((c=fis.read())!=-1)) break;
+                if (!((c=fis.read(buff))!=-1)) break;
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
             try {
-                fos.write(c);
+                fos.write(buff,0,c);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -34,5 +36,7 @@ public class copytest {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        long end=System.currentTimeMillis();
+        System.out.println("复制文件耗时："+(end-start)+"ms");
     }
 }
