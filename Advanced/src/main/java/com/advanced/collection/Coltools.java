9a404d9942e58b4570b3b5faa480e31692222735
safetyshare <vivo2198256468@gmail.com>
2026-05-18 10:50:52 +0800

    IO流

--- Advanced/src/main/java/com/advanced/collection/Coltools.java
+++ Advanced/src/main/java/com/advanced/collection/Coltools.java
@@ -3,7 +3,7 @@ package com.advanced.collection;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Contools {
+public class Coltools {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         Collections.addAll(list, 1,2,3,4,6);
---
+++ Advanced/src/main/java/com/advanced/io/ReadFile.java
@@ -0,0 +1,27 @@
+package com.advanced.io;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ReadFile {
+    public static void main(String[] args) {
+
+        // 1.创建文件输入流来读取文件
+        try {
+            FileInputStream in =new FileInputStream("src\\main\\resources\\test.txt") ;
+            int b=0;
+            // 2.通过循环读取文件内容，当返回-1时结束
+            while ((b=in.read()) != -1){
+                System.out.println(b);
+            }
+            // 3.关闭流
+            in.close();
+        }catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
