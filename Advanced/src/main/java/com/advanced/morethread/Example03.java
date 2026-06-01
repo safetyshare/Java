9dd67c2935f9d36e5d6530b87fb5cb5fceab5d9a
safetyshare <vivo2198256468@gmail.com>
2026-06-01 17:33:19 +0800

    多线程预习

--- Advanced/src/main/java/com/advanced/morethread/Example03.java
+++ Advanced/src/main/java/com/advanced/morethread/Example03.java
@@ -2,19 +2,22 @@ package com.advanced.morethread;
 
 public class Example03 {
     static void main() {
-        new TicketWindow().start();
-        new TicketWindow().start();
-        new TicketWindow().start();
-        new TicketWindow().start();
+        TicketWindow tw=new TicketWindow();
+        new Thread(tw,"窗口 1").start();
+        new Thread(tw,"窗口 2").start();
+        new Thread(tw,"窗口 3").start();
+        new Thread(tw,"窗口 4").start();
     }
 }
-class TicketWindow extends Thread {
+class TicketWindow implements Runnable {
     private int tickets = 100;
     public void run() {
         while (true) {
             if (tickets > 0) {
-                System.out.println(Thread.currentThread().getName()
-                        + " 正在发售第 " + tickets-- + " 张票 ");
+                System.out.println(Thread.currentThread().getName() + " 正在发售第 " + tickets-- + " 张票 ");
+            }else {
+                System.out.println(Thread.currentThread().getName()+"的票已经卖完");
+                System.exit(0);
             }
         }
     }
---
+++ Advanced/src/main/java/com/advanced/morethread/Example06.java
@@ -0,0 +1,30 @@
+package com.advanced.morethread;
+
+public class Example06 {
+    public static void main(String[] args) {
+        // 判断是否为后台线程
+        System.out.println("main线程是后台线程吗?"
+                + Thread.currentThread().isDaemon());
+        DamonThread dt = new DamonThread();
+        Thread thread = new Thread(dt, "后台线程");
+        System.out.println("thread线程默认是后台线程吗?" + thread.isDaemon());
+        // 将线程thread线程对象设置为后台线程
+        thread.setDaemon(true);
+        System.out.println("thread线程是后台线程吗?" + thread.isDaemon());
+        thread.start();
+        // 模拟主线程main的执行任务
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
+class DamonThread implements Runnable{
+    @Override
+    public void run() {
+        while (true){
+            System.out.println(Thread.currentThread().getName() + "---在运行");
+
+        }
+    }
+}
